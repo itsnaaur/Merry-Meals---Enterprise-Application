20beaff32707f70fb723086bca8b95cd593503e8
Eliazer Jevon <jevonloelan@gmail.com>
2022-10-01 21:45:59 +0800

    A little part of admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,12 +9,29 @@ class ManageController extends Controller
     //Admin Dashboard
     public function index()
     {
+        // $memberData = Member::paginate(4);
+        // dd($member);
         return view('Users.Admin.allMembers');
     }
 
     //All Partners
     public function allPartners(){
         return view('Users.Admin.allPartners');
+    }
+
+    //All Volunteers
+    public function allVolunteers(){
+        return view('Users.Admin.allVolunteers');
+    }
+
+    //All Donors
+    public function allDonors(){
+        return view('Users.Admin.allDonors');
+    }
+
+    //All Deliveries
+    public function allDeliveries(){
+        return view('Users.Admin.allDeliveries');
     }
 
     //Delete User
