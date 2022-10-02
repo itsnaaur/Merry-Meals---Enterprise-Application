a3a357f68ec7012e87b80c89f3776952e84aadb9
ZekaCedar <zulaikha.affandi@gmail.com>
2022-10-03 02:15:12 +0800

    new routing for member

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PartnerController;
 
 /*
@@ -57,6 +58,12 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::group(['prefix' => 'member'], function () {
     Route::get('/', [MemberController::class, 'index'])->name('member#index'); //member dashboard 
     Route::get('/member/{id}', [MemberController::class, 'saveMemberMealPlan'])->name('member#saveMemberMealPlan');
+    Route::get('/viewAllMenu', [MemberController::class, 'viewAllMenu'])->name('member#viewAllMenu');
+    Route::get('/viewMenu/{id}', [MemberController::class, 'viewMenu'])->name('member#viewMenu');
+    Route::get('/foodSafety', [MemberController::class, 'foodSafety'])->name('member#foodSafety');
+    Route::get('/orderConfirmation/{partner_id}/{menu_id}/{user_id}', [MemberController::class, 'orderConfirmation'])->name('member#orderConfirmation');
+    Route::post('/saveOrder', [OrderController::class, 'saveOrder'])->name('order#saveOrder');
+    Route::get('/showOrderDelivery/{id}', [OrderController::class, 'showOrderDelivery'])->name('order#showOrderDelivery');
 });
 
 //Partner
@@ -67,7 +74,7 @@ Route::group(['prefix' => 'partner'], function () {
     Route::get('/viewMenu/{id}', [PartnerController::class, 'viewMenu'])->name('partner#viewMenu'); //partner view a specific menu
     Route::get('/deleteMenu/{id}', [PartnerController::class, 'deleteMenu'])->name('partner#deleteMenu'); //deleting a specific menu
     Route::get('/updateMenu/{id}', [PartnerController::class, 'updateMenu'])->name('partner#updateMenu'); //calling the updation page
-    Route::post('/saveUpdate/{id}', [PartnerController::class, 'saveUpdate'])->name('partner#saveUpdate'); //saving the updated data
+    Route::post('/saveUpdate', [PartnerController::class, 'saveUpdate'])->name('partner#saveUpdate'); //saving the updated data
 });
 
 //Volunteer
