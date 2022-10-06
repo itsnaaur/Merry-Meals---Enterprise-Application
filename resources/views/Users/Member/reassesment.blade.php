fa8f7d2a0492cb26301277d32da3f4b051fa3356
Eliazer Jevon <jevonloelan@gmail.com>
2022-10-07 00:23:15 +0800

    Change the title of the button to Send not update

--- resources/views/Users/Member/reassesment.blade.php
+++ resources/views/Users/Member/reassesment.blade.php
@@ -5,7 +5,7 @@
 @extends('Users.Member.layouts.app')
 
 @section('content')
-		
+
 		<div id="fh5co-services-section">
 			{{-- <div class="container">
 				<div class="row">
@@ -27,14 +27,14 @@
                                 <h3>Reassesment</h3>
                                 <form action="{{ route('member#newReassesment', Auth()->user()->id) }}" method="POST">
                                     @csrf
-                                    
+
                                     {{-- REASON FOR REASSESMNET --}}
                                         <label class="userManagement">Why you want to extend the time of receiving this charity?</label><br>
                                         <textarea rows="5" name="member_extends_reason" class="input-md col-md-12" type="text">{{ old('member_extends_reason', $memberData->member_extends_reason) }}</textarea><br><br><br>
                                     {{-- END OF REASSESMENT REASON --}}
 
-                                    <div class="text-center"> 
-                                        <button class="btn-primary">Update</button> &nbsp;
+                                    <div class="text-center">
+                                        <button class="btn-primary">Send</button> &nbsp;
                                         <a href="{{ route('member#index') }}">Cancel</a>
                                     </div>
                                 </form>
